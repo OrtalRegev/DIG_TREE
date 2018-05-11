804e14da1d68708de8936942a2e4d9773f13593a
iftachfridental <ifridental58@gmail.com>
2018-05-11 17:51:09 +0300

    Working

--- main.py
+++ main.py
@@ -1,12 +1,56 @@
-import socket
+import dns
+import dns.name
+import dns.query
 import dns.resolver
 
-# Basic query
-for rdata in dns.resolver.query('www.yahoo.com', 'CNAME'):
-    print(rdata.target)
 
-# Set the DNS Server
-resolver = dns.resolver.Resolver()
-resolver.nameservers=[socket.gethostbyname('ns1.cisco.com')]
-for rdata in resolver.query('www.yahoo.com', 'CNAME'):
-    print(rdata.target)
+def get_authoritative_nameserver(domain, log=lambda msg: None):
+    n = dns.name.from_text(domain)
+
+    depth = 2
+    default = dns.resolver.get_default_resolver().nameservers[0]
+    nameserver = default.nameservers[0]
+
+    last = False
+    while not last:
+        s = n.split(depth)
+
+        last = s[0].to_unicode() == u'@'
+        sub = s[1]
+
+        log('Looking up %s on %s' % (sub, nameserver))
+        query = dns.message.make_query(sub, dns.rdatatype.NS)
+        response = dns.query.tcp(query, nameserver)
+
+        rcode = response.rcode()
+        if rcode != dns.rcode.NOERROR:
+            if rcode == dns.rcode.NXDOMAIN:
+                raise Exception('%s does not exist.' % sub)
+            else:
+                raise Exception('Error %s' % dns.rcode.to_text(rcode))
+
+        rrset = None
+        if len(response.authority) > 0:
+            rrset = response.authority[0]
+        else:
+            rrset = response.answer[0]
+
+        rr = rrset[0]
+        if rr.rdtype == dns.rdatatype.SOA:
+            log('Same server is authoritative for %s' % sub)
+        else:
+            authority = rr.target
+            log('%s is authoritative for %s' % (authority, sub))
+            nameserver = default.query(authority).rrset[0].to_text()
+
+        depth += 1
+
+    return nameserver
+
+
+def log(msg):
+    print msg
+
+
+print get_authoritative_nameserver("www.facebook.com", log)
+
